fe92b97f6df1137e989d069053a8bb3f9c1d44f9
Will Smith <panwarrishi96@gmail.com>
2019-07-21 18:51:27 +0530

    Commit

--- verloop.py
+++ verloop.py
@@ -1,1 +1,15 @@
 '''
+
+    This file contains an API that may be used for collaborative story writing.
+    Backend   - Python
+    Database  - MongoDB
+    Framework - Flask
+    Library   - PyMongo
+
+'''
+
+import pymongo
+from flask import Flask, request,jsonify
+from flask_restful import Resource, Api
+import requests
+from datetime import datetime
