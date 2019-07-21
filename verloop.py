357923f20dc418ee46e854b6d61a91aee62099ca
Will Smith <panwarrishi96@gmail.com>
2019-07-21 19:02:00 +0530

    parameters and post data test

--- verloop.py
+++ verloop.py
@@ -10,6 +10,24 @@
 
 import pymongo
 from flask import Flask, request,jsonify
-from flask_restful import Resource, Api
 import requests
 from datetime import datetime
+
+# DB connection
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+curr = myclient["mydb"]
+mycol = curr["stories2"]
+
+# Constraint Constants
+S_LENGTH = 2            # Sentence length
+P_LENGTH = 2            # Paragraph Length
+ST_LENGTH = 2           # Story Length
+
+# Flask app init
+app = Flask(__name__)
+
+# Add story
+@app.route('/add',methods=['POST'])
+def add_word():
+    data = request.get_json()
+    return data
